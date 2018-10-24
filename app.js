f27ada2b82c59695c4dac1ee2a00bf29567593f3
GLOBAL\I345149\Joey Yang <joey.yang02@sap.com>
2018-10-24 10:42:15 +0800

    remove front end module

--- app.js
+++ app.js
@@ -14,17 +14,19 @@ app.use(logger('dev'));
 app.use(express.json({limit:'100mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/client',express.static(path.join(__dirname+'/views', 'webapp')));
+app.use('/client',express.static(path.join(__dirname, 'webapp')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({limit:'100mb'}));
 app.use(bodyParser.urlencoded({ limit:'100mb', extended: true }));
 
 app.use('/client', serviceRequestRouter);
 
+/*
 // view engine setup
 app.engine('.html', ejs.__express);
 app.set('views', path.join(__dirname+'/views', 'webapp'));
 app.set('view engine', 'html');
+*/
 
 //cross domain
 app.all('*', function(req, res, next) {
