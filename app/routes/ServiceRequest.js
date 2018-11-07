32c7431075be4e72b6305e21ed1bfa49c5f3f58e
GLOBAL\I345149\Joey Yang <joey.yang02@sap.com>
2018-11-07 14:52:07 +0800

    clean up some codes

--- app/routes/ServiceRequest.js
+++ app/routes/ServiceRequest.js
@@ -8,7 +8,6 @@ const contactCommunicationHandler = require(process.cwd() + '/app/handler/Contac
 router.get('/getC4CContact',contactCommunicationHandler.getContactCommunicationData);
 router.get('/getServicePriorityCode',serviceRequestHandler.getServicePriorityCode);
 router.get('/getServiceRequestLifeCycleStatusCode',serviceRequestHandler.getServiceRequestLifeCycleStatusCode);
-router.get('/getServiceRequestDescription', serviceRequestHandler.getServiceRequestDescription);
 router.get('/getServiceRequestsCount', serviceRequestHandler.getServiceRequestsCount);
 router.get('/getServiceRequests', serviceRequestHandler.getServiceRequests);
 router.get('/getServiceIssueCategoryCatalogueCategory',serviceRequestHandler.getServiceIssueCategoryCatalogueCategory);
